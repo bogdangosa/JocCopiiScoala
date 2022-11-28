690d24681b2d82f4af8752ad57605a1ff1277cdf
Cosmin Cosma <cosmincosma04@gmail.com>
2022-11-28 12:12:39 +0200

    test cosmin

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test GitHub!</Text>
+      <Text>Test2.0 GitHub!</Text>
       <StatusBar style="auto" />
     </View>
   );
