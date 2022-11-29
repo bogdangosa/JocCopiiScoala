af8db2a6087f0c61e3e5cdb2757bf545a745a8de
Cosmin Cosma <cosmincosma04@gmail.com>
2022-11-29 08:53:44 +0200

    pozeSimpleCard

--- components/cards/SimpleCard.js
+++ components/cards/SimpleCard.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, Touchable, View } from 'react-native';
+import { StyleSheet, Text, Touchable, View , Image} from 'react-native';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const SimpleCard = ({text,image,color,onPress})=>{
 
     return(
-        <Pressable style={[styles.SimpleCard,{borderColor:color}]} onPress={onPress}>
-            <Text style={[styles.SimpleCardText,{color:color}]}>{text}</Text>
+        <Pressable style={[styles.SimpleCard,{borderColor:color, backgroundColor:color,}]} onPress={onPress}>
+            <Text style={[styles.SimpleCardText,{color:"#fff"}]}>{text}</Text>
+            <Image style={styles.SimpleCardImage } source={image}/>
         </Pressable>
     )
 }
@@ -15,12 +16,21 @@ const styles = StyleSheet.create({
         width:"100%",
         borderWidth:2,
         borderRadius:10,
-        padding:25,
+        paddingLeft:25,
         marginBottom:20,
-        backgroundColor:"#fff",
+        flexDirection: "row",
+        alignItems: "flex-end",
+        justifyContent: "space-between",
+        overflow: "hidden",
     },
     SimpleCardText:{
-        fontSize:20,
+        fontSize:25,
+        marginBottom: 10,
+    },
+    SimpleCardImage:{
+        height:80,
+        width: 80,
+        backgroundColor: '#fff',
     },
 });
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,9 +6,9 @@ const HomeScreen = ({navigation})=>{
     return(
         <View style={styles.HomeScreen}>
             <View style={styles.GameListContainer}>
-                <SimpleCard text="Recunoastere"color="#6C63FF" onPress={() =>{ navigation.navigate('Games Menu',{title:"Recunoastere"}) }}/>
-                <SimpleCard text="Reordoneaza"color="#2FEA63" onPress={() =>{ navigation.navigate('Games Menu',{title:"Reordoneaza"})}}/>
-                <SimpleCard text="Selecteaza" color="#EA9F2F" onPress={() =>{ navigation.navigate('Games Menu',{title:"Selecteaza"})}}/>
+                <SimpleCard text="Recunoastere"color="#6C63FF" image={require('../assets/Panda6.png')} onPress={() =>{ navigation.navigate('Games Menu',{title:"Recunoastere"}) }}/>
+                <SimpleCard text="Reordoneaza"color="#2FEA63" image={require('../assets/Panda5.png')} onPress={() =>{ navigation.navigate('Games Menu',{title:"Reordoneaza"})}}/>
+                <SimpleCard text="Selecteaza" color="#EA9F2F" image={require('../assets/Panda3.png')} onPress={() =>{ navigation.navigate('Games Menu',{title:"Selecteaza"})}}/>
             </View>
         </View>
     )
