f8a49b1d75d826472be13ef4c8f05960f6ec9c26
Cosmin Cosma <cosmincosma04@gmail.com>
2022-12-07 14:37:48 +0200

    dbRecunoastereGame_ElementeRandom

--- games/RecunoastereGame.js
+++ games/RecunoastereGame.js
@@ -3,23 +3,50 @@ import { useEffect, useState } from "react";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 import SelectableButton from "../components/buttons/SelectableButton";
 import {colors} from "../themes/color";
+import * as SQLite from 'expo-sqlite';
 
-const RecunoastereGame = ({field}) => {
+
+const RecunoastereGame = ({field, onVerify}) => {
     const [butoane,setbutoane]=useState([false,false,false,false])
+    const [Variante,setVariante]=useState(["Cal","Câine","Lup","Vaca"])
+    const DatabaseName = "MainDatabase";
+    const tableName = "MainTable";
+    const db = SQLite.openDatabase(DatabaseName);
+    
+    useEffect(() => {
+      //se apeleaza cand apesi butonul de verificare
+      Verify();
+    }, [onVerify]);
 
-  
+    useEffect (()=>{
+      getVarinte();
+      var Solution;
+      Solution="Lup";
+    },[]);
+
+    const getVarinte = () =>{
+      db.transaction(tx => {
+        tx.executeSql(`SELECT * FROM ${tableName} ORDER BY random() LIMIT 4`, null, 
+        (txObj, ResultsSet) => setVariante(ResultsSet.rows._array),
+        (txObj, error) => console.log('Error ', error)
+        );
+      }) // end transaction
+    }
+    const Verify=()=>{
+      
+    }
 
   return (
     <View style={styles.RecunoastereGame} >
       <Text style={styles.text}>Ce {field} este ?</Text>
       <Image style={styles.imagine} source={require("../assets/lup.png")}/>
       <View style={styles.poz_butoane}>
-      <SelectableButton button_state={butoane[0]} onPress={()=>setbutoane([true,false,false,false])} more_styles={styles.buton}>Cal</SelectableButton>
-      <SelectableButton button_state={butoane[1]} onPress={()=>setbutoane([false,true,false,false])} more_styles={styles.buton}>Caine</SelectableButton>
+      <SelectableButton button_state={butoane[0]} onPress={()=>setbutoane([true,false,false,false])} more_styles={styles.buton}>{Variante[0].name}</SelectableButton>
+      <SelectableButton button_state={butoane[1]} onPress={()=>setbutoane([false,true,false,false])} more_styles={styles.buton}>{Variante[1].name}</SelectableButton>
       </View>
       <View style={styles.poz_butoane}>
-      <SelectableButton button_state={butoane[2]} onPress={()=>setbutoane([false,false,true,false])} more_styles={styles.buton}>Lup</SelectableButton>
-      <SelectableButton button_state={butoane[3]} onPress={()=>setbutoane([false,false,false,true])} more_styles={styles.buton}>Vaca</SelectableButton>
+      <SelectableButton button_state={butoane[2]} onPress={()=>setbutoane([false,false,true,false])} more_styles={styles.buton}>{Variante[2].name}</SelectableButton>
+      <SelectableButton button_state={butoane[3]} onPress={()=>setbutoane([false,false,false,true])} more_styles={styles.buton}>{Variante[3].name}</SelectableButton>
       </View>
     </View>
   );
